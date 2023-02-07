c6c1b55d841c14a6e342c6077644a6f4e60a238f
GeonwooVincentKim <kdsnop@gmail.com>
2023-02-07 11:20:30 +0900

    [Update] - Several colors

--- lib/widgets/heatmap/heatmap_summary.dart
+++ lib/widgets/heatmap/heatmap_summary.dart
@@ -4,9 +4,10 @@ import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
 
 class HeatmapSummary extends StatelessWidget {
   final Map<DateTime, int> datasets;
-  final String startDate;
+  // final String startDate;
 
-  const HeatmapSummary({super.key, required this.datasets, required this.startDate});
+  // const HeatmapSummary({super.key, required this.datasets, required this.startDate});
+  const HeatmapSummary({super.key, required this.datasets});
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +27,16 @@ class HeatmapSummary extends StatelessWidget {
         scrollable: true,
         size: 30,
         colorsets: const {
-          1: Color.fromARGB(50, 2, 179, 8),
+          1: Color.fromARGB(20, 2, 179, 8),
+          2: Color.fromARGB(40, 2, 179, 8),
+          3: Color.fromARGB(60, 2, 179, 8),
+          4: Color.fromARGB(80, 2, 179, 8),
+          5: Color.fromARGB(100, 2, 179, 8),
+          6: Color.fromARGB(120, 2, 179, 8),
+          7: Color.fromARGB(150, 2, 179, 8),
+          8: Color.fromARGB(180, 2, 179, 8),
+          9: Color.fromARGB(220, 2, 179, 8),
+          10: Color.fromARGB(255, 2, 179, 8),
         },
       )
     );
